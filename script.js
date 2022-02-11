aef1b6b486af2cae0fd290b855f3c7cc11500ab1
Saad Evara <saadevara@gmail.com>
2022-02-11 16:09:31 +0530

    Picture in Picture End

--- script.js
+++ script.js
@@ -8,12 +8,16 @@ async function selectMediaStream() {
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     videoElement.srcObject = mediaStream;
     videoElement.onloadedmetadata = () => {
-      videoElement.play();
+        videoElement.play();
     };
   } catch (error) {}
 }
 
-button.addEventListener("click", selectMediaStream);
+button.addEventListener("click", async () => {
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
 
 // on load
 selectMediaStream();
